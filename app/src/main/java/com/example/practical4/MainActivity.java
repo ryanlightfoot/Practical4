448c41e2c1af54fdac883410e7fa5cf7dba76c5a
ryanlightfoot <ryanlightfoot509@gmail.com>
2023-03-26 13:57:55 +0200

    commit 3 - added Toast Validation checking for amtPaid to be above 30% of amtOwed

--- app/src/main/java/com/example/practical4/MainActivity.java
+++ app/src/main/java/com/example/practical4/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,24 +23,52 @@ public class MainActivity extends AppCompatActivity {
         RadioButton rgbY = (RadioButton) findViewById(R.id.rgpYes);
         RadioButton rgbN = (RadioButton) findViewById(R.id.rgpNo);
 
+        TextView txvStNo = (TextView) findViewById(R.id.txbStudentNo);
+        String numb = getString(R.string.num);
+        final double amtOwe = 25000.00; // How much needs to be paid
+
+        //Error messages
+        Toast tReg = Toast.makeText(getApplicationContext(),"Please pay registration", Toast.LENGTH_SHORT);
+        Toast tStNo = Toast.makeText(getApplicationContext(),"Please Enter Student Number", Toast.LENGTH_SHORT);
+        Toast tStLied = Toast.makeText(getApplicationContext(),"You lied... Please pay.", Toast.LENGTH_SHORT);
+
+
+        double amtPaid = (double) Double.parseDouble(numb); //Get the amount the student has paid
+
+
+
         rgbY.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                double regOwe = amtOwe * 0.3; // How much needs to be paid to register
 
+                if(amtPaid<regOwe) // Check if Registration amount has been paid
+                {
+                    rgbN.setChecked(true);
+                    tStLied.show();
+                }
             }
         });
 
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Register.class));
+                if(!rgbY.isChecked()){
+                    tReg.show(); //Check if paid registration
+                }
+                else if (txvStNo.getText().toString().equals("")) {
+                    tStNo.show(); //Check if entered student number
+                }
+                else {
+                    startActivity(new Intent(MainActivity.this, Register.class)); //Goes to new instance
+                }
             }
         });
 
         btnPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, PayScreen.class));
+                startActivity(new Intent(MainActivity.this, PayScreen.class)); //Goes to new instance
             }
         });
     }
